140496787318460ee88842d3597893e4f0b18c26
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-13 15:34:30 -0300

    Modified index method, added authenticate_user! method to tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,11 @@
 class TweetsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
     def index 
     end 
-    
+
     def new 
+        @tweet = Tweet.new
     end 
 
     def create 
