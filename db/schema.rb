518589d25a2b793dec6cac6cda9998fe1cc68ac0
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-13 15:18:24 -0300

    Ran migration to create tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_141023) do
+ActiveRecord::Schema.define(version: 2020_11_13_181315) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id"
+    t.integer "retweet", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
