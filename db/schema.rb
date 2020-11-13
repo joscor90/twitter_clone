46a252a9044d4c3e09367829f1e80d8e4267c28e
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-13 11:12:19 -0300

    Ran a migration to add a constraint of not null to name column in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_135550) do
+ActiveRecord::Schema.define(version: 2020_11_13_141023) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_135550) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.text "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
