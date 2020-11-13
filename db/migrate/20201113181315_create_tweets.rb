6b4164958966a929bf9598a490873ae48ed5875a
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-13 15:17:52 -0300

    Modified the migration of create tweets

--- db/migrate/20201113181315_create_tweets.rb
+++ db/migrate/20201113181315_create_tweets.rb
@@ -1,9 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.text :content
+      t.text :content, null: false
       t.references :user, foreign_key: true
-      t.integer :retweet
+      t.integer :retweet, default: 0
 
       t.timestamps
     end
