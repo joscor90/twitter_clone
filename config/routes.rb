fbe9ed290462d47ac494e7bcff67576e0aefa175
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-13 15:20:19 -0300

    Modified routes.rb adding resources routes for tweets

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root 'home#index'
+  resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
