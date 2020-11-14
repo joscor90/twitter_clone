4d2d369e34602be21fb845b8d6b2a9537bee4d92
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-14 18:46:21 -0300

    Added Kaminari to the Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :tweets
-  has_many :retweets
+  has_many :tweets, dependent: :destroy
+  has_many :retweets, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
