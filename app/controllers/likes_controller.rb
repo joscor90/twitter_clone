16a58683134b55cc1ce998d1873b813e78abdb9b
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-15 11:28:54 -0300

    Generated create method and like params in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,15 @@
 class LikesController < ApplicationController
     def create 
+        @like = Like.create(like_params)
+        @like.user_id = current_user.id 
+        if @like.save
+            redirect_to root_path
+        else
+            render root_path, notice: "Couldn't give a like"
+        end
+    end
+
+    def like_params
+        params.require(:like).permit(:id)
     end
 end
