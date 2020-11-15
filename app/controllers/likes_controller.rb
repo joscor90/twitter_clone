6a7166356d928066a7804a106cbaa2f90ddcf7df
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-15 11:35:30 -0300

    Modified create method in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,11 @@
 class LikesController < ApplicationController
     def create 
-        @like = Like.create(like_params)
+        @like = Like.create(tweet_id: params[:id])
         @like.user_id = current_user.id 
         if @like.save
             redirect_to root_path
         else
             render root_path, notice: "Couldn't give a like"
         end
-    end
-    
-    private
-    def like_params
-        params.require(:like).permit(:id)
     end
 end
