c14dcf768657b21ffab918123e024f35eb5c2400
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-15 11:25:50 -0300

    Modified routes.rb adding a route to create a like

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'tweets#index'
   resources :tweets
 
+  get 'like/:id', to: "likes#create", as: "create_like"
   get 'retweets/:id', to: 'retweets#index', as: 'retweets'
   get 'retweet/:id', to: 'tweets#retweet', as: 'retweet'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
