386122ab0006d719ba2332001b75cf765d3f1107
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-16 15:41:38 -0300

    Added create action to retweets controller, modify form in new.html.erb of retweet

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -9,6 +9,18 @@ class RetweetsController < ApplicationController
     end
 
     def create
+        @tweet = Tweet.create(tweet_params)
+        @tweet.user_id = current_user.id
+        if @tweet.save
+            @retweet = Retweet.create(user_id: current_user.id, tweet_id: @tweet.id)
+            @retweet.save 
+            redirect_to root_path, notice: "Tweet was created successfully"
+        else 
+            render :new, notice: "Tweet couldn't be created"
+        end
+    end
 
+    def tweet_params 
+        params.require(:retweet).permit(:content)
     end
 end
