47acc6cc814c3f190530062276a5a3a146aaca07
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-16 15:29:11 -0300

    Ran migration to add foregin key to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_141110) do
+ActiveRecord::Schema.define(version: 2020_11_16_182710) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_11_15_141110) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "retweet_id"
+    t.index ["retweet_id"], name: "index_tweets_on_retweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
