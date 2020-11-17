ad8272a62c42bedc45addf621651d7a1c381f222
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-17 10:33:06 -0300

    Defined a method in applications controller to look for a reference tweet. Modified index view of tweetsalong this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    attr_reader :ref_tweet
+
+    def search_tweet(t)
+        ref_retweet = Retweet.find_by(id: t.retweet_id)
+        @ref_tweet = Tweet.find_by(id: ref_retweet.tweet_id)
+        return @ref_tweet
+    end
+
+    helper_method :search_tweet
 end
