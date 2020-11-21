065c78e5f99b398a9a944fa36802e5dd9f84f01b
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-21 12:00:04 -0300

    Defined create action in friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,3 +1,11 @@
 class FriendsController < ApplicationController
-    
+
+    def create 
+        followed_user = params.require(:user_id)
+        followed_user_name = User.find_by(id: followed_user)
+        @friend = Friend.create(user_id: current_user.id, friend_id: followed_user)
+        if @friend.save
+            redirect_to root_path, notice: "You are know following #{followed_user_name}"
+        end
+    end
 end
