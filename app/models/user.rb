570ea982743337681791e903af92b0a3753a239c
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-21 12:05:46 -0300

    Modified user model  to specify the relationship with friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :retweets, dependent: :destroy
   has_many :likes
+  has_many :friends
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
