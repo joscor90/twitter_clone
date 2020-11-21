9bb5ae1c029627fc7b7b50b563816bbed897eb01
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-21 14:57:53 -0300

    Fixed an error in create action in friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
     def create 
         friend_id = params.require(:user_id)
-        user_to_be_friend = User.find_by(id: followed_user)
+        user_to_be_friend = User.find_by(id: friend_id)
         @friend = Friend.create(user_id: current_user.id, friend_id: user_to_be_friend.id)
         if @friend.save
             redirect_to root_path, notice: "#{user_to_be_friend.name} is your friend!"
