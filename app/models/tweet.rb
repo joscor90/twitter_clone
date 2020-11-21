1cea0b9419bfdb396c8ee42b044030fdc6811f22
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-21 15:10:23 -0300

    Added first version of scope tweets_for_me

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
   #Aqui va el scope
+  scope :tweets_for_me, -> { where('user_id == ?', current_user.friends.pluck(:friend_id) }
+
   belongs_to :user
   has_many :retweets
   has_many :likes
