cdf09685f6ae07867a051074b4ea3b838ef5f19b
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-22 14:20:12 -0300

    Modified create action in friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,8 +3,6 @@ class FriendsController < ApplicationController
         friend_id = params.require(:user_id)
         user_to_be_friend = User.find_by(id: friend_id)
         @friend = Friend.create(user_id: current_user.id, friend_id: user_to_be_friend.id)
-        if @friend.save
-            redirect_to root_path, notice: "#{user_to_be_friend.name} is your friend!"
-        end
+        @friend.save
     end
 end
